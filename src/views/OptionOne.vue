091a0710db4fcb3350f9fabf0e6701103d0cf244
Samuel <velizsamuel50@gmail.com>
2024-05-10 19:42:49 -0400

    feat: tailwind class

--- src/views/OptionOne.vue
+++ src/views/OptionOne.vue
@@ -3,7 +3,7 @@ import useReportes from '../composables/useReportes';
 import CardInfoUser from '../components/CardInfoUser.vue';
 
 
-const { search, inputValue, insertValue, changeValue, result, hidden, popupStatus, popup, alerta} = useReportes()
+const { search, inputValue, insertValue, changeValue, result, hidden, popupStatus, popup, alerta } = useReportes()
 </script>
 
 <template>
@@ -11,30 +11,30 @@ const { search, inputValue, insertValue, changeValue, result, hidden, popupStatu
 
   <main id="reportes">
     <section
-    class="absolute bg-black bg-opacity-30 w-full h-full z-50 grid place-content-center top-0 left-0 cursor-pointer"
-    @click="(e) => hidden(e)" ref="popup">
-    <img class="h-90 bg-white rounded-lg fade" src="@/assets/msg.jpeg" alt="" v-if="popupStatus == 0">
+      class="fixed bg-black bg-opacity-30 w-full h-full z-50 grid place-content-center place-items-center top-0 left-0 cursor-pointer"
+      @click="(e) => hidden(e)" ref="popup">
+      <img class="h-4/5 rounded-lg fade" src="@/assets/msg.jpeg" alt="" v-if="popupStatus == 0">
 
-    <article class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-96 fade" v-else-if="popupStatus == 1">
-      <h3 class="text-slate-900 dark:text-white text-base font-medium capitalize mx-auto p-10">Ingrese la hora del voto:</h3>
-      <input type="datetime-local" name="" id="">
-      <button class=" bg-[#ECA008] hover:bg-[#010c41] text-white font-bold py-2 px-10 rounded-3xl mx-auto block my-5" @click="() => alerta('Enviado', 'se ha enviado el registro', 'success')">Registrar</button>
-    </article>
+      <article class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-96 fade"
+        v-else-if="popupStatus > 1">
+        <h3 class="text-slate-900 dark:text-white text-base font-medium capitalize mx-auto p-10">Ingrese la hora del
+          voto:</h3>
+        <input type="datetime-local" name="" id="">
+        <div v-if="popupStatus == 2">
+          <h3 class="text-slate-900 dark:text-white text-base font-medium capitalize mx-auto p-5">Justifique:</h3>
+          <input type="text" placeholder="justifique...">
+        </div>
+        <button class=" bg-[#ECA008] hover:bg-[#010c41] text-white font-bold py-2 px-10 rounded-3xl mx-auto block my-5"
+          @click="() => alerta('Enviado', 'se ha enviado el registro', 'success')">Registrar</button>
+      </article>
 
-    <article class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-96 fade" v-else-if="popupStatus == 2">
-      <h3 class="text-slate-900 dark:text-white text-base font-medium capitalize mx-auto py-10">Ingrese la hora del voto:</h3>
-      <input type="datetime-local" name="" id="">
-      <h3 class="text-slate-900 dark:text-white text-base font-medium capitalize mx-auto p-5">Justifique:</h3>
-      <input type="text" placeholder="justifique...">
-      <button class=" bg-[#ECA008] hover:bg-[#010c41] text-white font-bold py-2 px-10 rounded-3xl mx-auto block my-5" @click="() => alerta('Enviado', 'se ha enviado el registro', 'success')">Registrar</button>
-    </article>
-  </section>
+    </section>
 
     <img alt="Vue logo" src="@/assets/logo.svg" class="mx-auto h-48" />
     <form @submit.prevent="search">
       <label>Ingrese una cedula: </label>
       <article id="busqueda">
-        <select class="w-20" name="tipopersona" @change="insertValue" disabled>
+        <select class="w-20 bg-white" name="tipopersona" @change="insertValue" disabled>
           <option value="V">V-</option>
           <option value="E">E-</option>
         </select>
@@ -53,28 +53,28 @@ const { search, inputValue, insertValue, changeValue, result, hidden, popupStatu
       <CardInfoUser :result="[result[0].id]" title="Registro" icon="fa-solid fa-check-to-slot" :hidden="hidden" />
 
     </section>
-
+    <section class="grid grid-cols-2 h-3.5" v-else>
+      <article class="w-full bg-[#010c41] rounded-r-full grid">
+        <h2 class="place-self-center text-white text-3xl px-10 py-10">Ingrese su numero de cedula para registrar el
+          voto.</h2>
+      </article>
+      <img class="w-auto h-auto" src="@/assets/banner.svg" alt="">
+    </section>
   </main>
 
-  <section id="banner" v-if="Object.keys(result).length == 0">
-        <article>
-              <h2>Ingrese su numero de cedula para registrar el voto.</h2>
-        </article>
-            <img class="w-auto h-auto" src="@/assets/banner.svg" alt="">
-    </section>
+
 </template>
 
 
 <style scoped>
-
-#reportes{
-  background-image:url("@/assets/bg.svg");
-  min-height: 100vh;
+#reportes {
+  background-image: url("@/assets/bg.svg");
   background-repeat: no-repeat;
   background-position: center;
   background-size: 100% 100%;
   background-attachment: fixed;
 }
+
 #banner {
   display: grid;
   position: absolute;
@@ -85,34 +85,24 @@ const { search, inputValue, insertValue, changeValue, result, hidden, popupStatu
   margin-left: -120px;
 }
 
-#banner article:first-child{
+#banner article:first-child {
   background-color: #010c41;
   width: 980px;
 }
-#banner article{
+
+#banner article {
   color: white;
   height: 80%;
   border-radius: 0% 30% 30% 0%;
   display: grid;
   place-items: center;
   padding: 30px
-  
 }
 
-#banner article h2{
+#banner article h2 {
   font-size: 50px;
   font-weight: bold;
 }
-
-#banner article img{
-  justify-items: left;
-}
-
-
-#banner article img{
-  justify-items: left;
-}
-
 
 #FormContainer {
   width: 30%;
@@ -132,9 +122,9 @@ form {
 
 @media screen and (max-width : 768px) {
   form {
-  width: 80%;
-  margin: 0px auto;
-}
+    width: 80%;
+    margin: 0px auto;
+  }
 }
 
 form label {
@@ -197,7 +187,4 @@ form .cosultar:hover {
   grid-template-columns: repeat(auto-fill, minmax(380px, 1fr));
   gap: 20px;
 }
-
-
-
 </style>
